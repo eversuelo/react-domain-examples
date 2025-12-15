95f67cc3576bd1475f1fd8303434cb1ac0770692
Everard Torres <everardo.03.04.99@gmail.com>
2025-12-15 04:38:52 -0600

    refactor: Migrate project from `quisco-front` to `14-quiosco` directory, replacing old files with a new base setup.

---
+++ 14-quiosco/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Quiosco</h1>
+      </div>
+    </main>
+  );
+}
